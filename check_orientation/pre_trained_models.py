5d680cc577f41088db442e1268712c630c80e8e6
BCCC0 <52162254+BCCC0@users.noreply.github.com>
2024-03-10 16:52:02 +0800

    Update pre_trained_models.py

--- check_orientation/pre_trained_models.py
+++ check_orientation/pre_trained_models.py
@@ -8,6 +8,26 @@ from timm import create_model as timm_create_model
 from torch import nn
 from torch.utils import model_zoo
 
+import argparse
+from pathlib import Path
+from typing import Any, Dict, List, Optional
+
+import albumentations as albu
+import numpy as np
+import torch
+import torch.nn.parallel
+import torch.utils.data
+import torch.utils.data.distributed
+import yaml
+from albumentations.core.serialization import from_dict
+
+from torch import nn
+from torch.utils.data import Dataset
+from torch.utils.data.distributed import DistributedSampler
+from tqdm import tqdm
+
+from check_orientation.pre_trained_models import create_model
+
 model = namedtuple("model", ["url", "model"])
 
 models = {
@@ -55,5 +75,92 @@ def create_model(model_name: str, activation: Optional[str] = "softmax") -> nn.M
         return nn.Sequential(model, nn.Softmax(dim=1))
 
     return model
+
+def main(input_path, output_path, batch_size=1, num_workers=4):
+    # torch.distributed.init_process_group(backend="nccl")
+    model = create_model("swsl_resnext50_32x4d")
+
+    with open(args.config_path) as f:
+        hparams = yaml.load(f, Loader=yaml.SafeLoader)
+
+    hparams.update(
+        {
+            "local_rank": args.local_rank,
+            "fp16": args.fp16,
+        }
+    )
+
+    args.output_path.mkdir(parents=True, exist_ok=True)
+    hparams["output_path"] = args.output_path
+
+    device = torch.device("cuda")  # pylint: disable=E1101
+
+
+    corrections: Dict[str, str] = {"model.": ""}
+    state_dict = state_dict_from_disk(file_path=args.weight_path, rename_in_layers=corrections)
+
+    model = model.to(device)
+
+    # model = torch.nn.parallel.DistributedDataParallel(
+    #     model, device_ids=[args.local_rank], output_device=args.local_rank
+    # )
+
+    file_paths = []
+
+    for regexp in ["*.jpg", "*.png", "*.jpeg", "*.JPG"]:
+        file_paths += sorted(args.input_path.rglob(regexp))
+
+    # Filter file paths for which we already have predictions
+    file_paths = [x for x in file_paths if not (args.output_path / x.parent.name / f"{x.stem}.txt").exists()]
+
+    dataset = InferenceDataset(file_paths, transform=from_dict(hparams["test_aug"]))
+
+    sampler = DistributedSampler(dataset, shuffle=False)
+
+    dataloader = torch.utils.data.DataLoader(
+        dataset,
+        batch_size=args.batch_size,
+        num_workers=args.num_workers,
+        pin_memory=True,
+        shuffle=False,
+        drop_last=False,
+        sampler=sampler,
+    )
+
+    predict(dataloader, model, hparams, device)
+
+
+def predict(dataloader, model, hparams, device):
+    model.eval()
+
+    if hparams["local_rank"] == 0:
+        loader = tqdm(dataloader)
+    else:
+        loader = dataloader
+
+    with torch.no_grad():
+        for batch in loader:
+            torched_images = batch["torched_image"]  # images that are rescaled and padded
+
+            if hparams["fp16"]:
+                torched_images = torched_images.half()
+
+            image_paths = batch["image_path"]
+
+            batch_size = torched_images.shape[0]
+
+            predictions = model(torched_images.to(device))
+
+            for batch_id in range(batch_size):
+                file_id = Path(image_paths[batch_id]).stem
+                folder_name = Path(image_paths[batch_id]).parent.name
+
+                prob = predictions[batch_id].cpu().numpy().astype(np.float16)
+
+                (hparams["output_path"] / folder_name).mkdir(exist_ok=True, parents=True)
+
+                with open(str(hparams["output_path"] / folder_name / f"{file_id}.txt"), "w") as f:
+                    f.write(str(prob.tolist()))
+                
 
 
